40402ec198faae519757940cc7ec2f2c92555a22
jocelyn castelettii <jocelyncasteletti>
2019-08-08 15:19:15 -0400

    se modifica estructura componente professionals y app-component para dar estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,19 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { NewsComponent } from './components/news/news.component';
+import { ConditionComponent } from './components/condition/condition.component';
+import { ProfessionalsComponent } from './components/professionals/professionals.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { AdminComponent } from './components/admin/admin.component';
+
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'news', component: NewsComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'condition', component: ConditionComponent },
+  { path: 'professionals', component: ProfessionalsComponent },
   { path: 'admin', component: AdminComponent },
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
