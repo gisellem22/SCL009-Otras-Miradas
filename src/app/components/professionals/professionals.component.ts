96a353d5d04fbde22f1289efb18f10f1fe30a86c
Giselle Machado <gisellemachadoyes@gmail.com>
2019-08-10 12:22:50 -0400

    array unique de specialties

--- src/app/components/professionals/professionals.component.ts
+++ src/app/components/professionals/professionals.component.ts
@@ -9,21 +9,27 @@ import { PRO } from 'src/app/models/professional';
 })
 export class ProfessionalsComponent implements OnInit {
   professionals: PRO[];
-  specialty: any;
-//   filterSpecialty() {
-//     this.specialty = this.professionals.filter(p => {
-//     if (p.specialty === "psicología"){
-//     p.specialty;
-//     }
-//   }
-// )
-// console.log(this.specialty)
-//     }
+  specialty: PRO[];
+  //areas: string[] = ["Broncopulmonar","Cirugía","Fisiatría","Fonoaudiología","Genética Clínica","Infectología","Kinesiología","Neurología","Psicología","Psicopedagogía","Terapia Ocupaciona"]
+  areas: string[] = [];
+
+  filterSpecialty() {
+    this.professionals.forEach(p => {
+      this.areas.push(p.specialty)
+    })
+    this.uniqueAreas()
+  }
+  uniqueAreas() {
+    this.areas = this.areas.filter((value, index, self) => self.indexOf(value) === index)
+    console.log(this.areas)
+  }
+
   constructor(public professionalService: ProfessionalService) { }
   ngOnInit() {
-  this.professionalService.getProfessionals().subscribe(professionals => {
+this.professionalService.getProfessionals().subscribe(professionals => {
       this.professionals = professionals;
-     // this.filterSpecialty();
+      console.log(this.professionals);
+      this.filterSpecialty();
     });
   }
   deleteProfessional($event, professional) {
