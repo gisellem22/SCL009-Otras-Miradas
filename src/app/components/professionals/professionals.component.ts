acb1b34f470e1d35b2c0b6e9442cb4dc41caea09
Giselle Machado <gisellemachadoyes@gmail.com>
2019-08-11 20:00:26 -0400

    se crea lista dinámica de  profesionales por area en component.professionals

--- src/app/components/professionals/professionals.component.ts
+++ src/app/components/professionals/professionals.component.ts
@@ -16,30 +16,30 @@ export class ProfessionalsComponent implements OnInit {
 
  //para imprimir categorias dinámicas
 
-  // arrayMap (array:any) {
-  //   console.log("array: ",array)
-  //   let arrayGroup = this.groupBy(array, "specialty");
-  //   let resultArray = [];
-  //   let keysIterable = Object.keys(arrayGroup);
+  arrayMap (array:any) {
+    console.log("array: ",array)
+    let arrayGroup = this.groupBy(array, "specialty");
+    let resultArray = [];
+    let keysIterable = Object.keys(arrayGroup);
   
-  //   for (let key of keysIterable) {
-  //     let obj = {
-  //       especialidad: key,
-  //       doctores: arrayGroup[key]
-  //     };
-  //     resultArray.push(obj);
-  //   }
+    for (let key of keysIterable) {
+      let obj = {
+        especialidad: key,
+        doctores: arrayGroup[key]
+      };
+      resultArray.push(obj);
+    }
   
-  //   return resultArray;
-  // }
+    return resultArray;
+  }
   
-  // groupBy (xs, key) {
-  //   console.log(xs)
-  //   return xs.reduce((rv, x) => {
-  //     (rv[x[key]] = rv[x[key]] || []).push(x);
-  //     return rv;
-  //   }, {});
-  // };
+  groupBy (xs, key) {
+    console.log(xs)
+    return xs.reduce((rv, x) => {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  };
   
 // para hacer el select
 
@@ -66,8 +66,11 @@ export class ProfessionalsComponent implements OnInit {
   ngOnInit() {
 this.professionalService.getProfessionals().subscribe(professionals => {
       this.professionals = professionals;
+      this.areas = this.arrayMap(this.professionals)
       console.log(this.professionals);
+      console.log("areas", this.areas)
     });
+
   }
 
   deleteProfessional($event, professional) {
